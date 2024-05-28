107fed5a6cbe3a3718365354a8cab3381e6330ae
Tomas Suarez Lissi <tomas.sl@hotmail.com>
2024-05-28 21:38:23 +0930

    avatar menu on nav bar

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,5 +1,19 @@
 import { Link, Form } from "@remix-run/react";
 import { UserWithRelations } from "~/models/user.server";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuPortal,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
 
 interface NavBarProps {
   user?: UserWithRelations;
@@ -7,19 +21,35 @@ interface NavBarProps {
 
 export const NavBar = ({ user }: NavBarProps) => {
   return (
-    <header className="flex items-center justify-between bg-slate-800 p-4 text-white">
+    <header className="flex items-center justify-between bg-slate-800 p-5 text-white">
       <h1 className="text-3xl font-bold">
         <Link to=".">Negrify</Link>
       </h1>
       {user ? (
-        <Form action="/logout" method="post">
-          <button
-            type="submit"
-            className="rounded bg-slate-600 px-4 py-2 text-blue-100 hover:bg-blue-500 active:bg-blue-600"
-          >
-            Salir
-          </button>
-        </Form>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <button className="h-16 w-16 mr-4 rounded-full">
+              <img
+                className="w-full rounded-full"
+                src={user.dj?.avatar || user.cliente?.avatar}
+              />
+            </button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="w-56">
+            <DropdownMenuItem>
+              <Link className="w-full" to="/perfil">
+                Perfil
+              </Link>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <Form className="w-full" action="/logout" method="post">
+                <button className="w-full text-left" type="submit">
+                  Log out
+                </button>
+              </Form>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
       ) : (
         <Link to="/login">
           <button
